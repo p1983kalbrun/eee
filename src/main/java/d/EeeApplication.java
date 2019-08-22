f3c90875f21e019059fe24ef0afa15dd49489c2b
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2019-08-22 16:47:38 +0100

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/d/EeeApplication.java
@@ -0,0 +1,12 @@
+package d;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeeApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(EeeApplication.class, args);
+	}
+
+}
